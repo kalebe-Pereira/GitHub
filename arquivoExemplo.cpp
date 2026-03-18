27cfedf1b1104d9a54ab9517da19cc39aae42eab
Kalebe Pereira <kalebepereira73@gmai.com>
2026-03-18 17:34:32 -0300

    Primeiro Commit

---
+++ arquivoExemplo.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Este é um arquivo de exemplo." << '\n';
+
+    std::cout << "Outra linha Adicionada" << "\n";
+
+    std::cout << "Linha adicionada na branch secundaria." << '\n';
+    
+    return 0;
+}
